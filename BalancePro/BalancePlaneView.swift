f706cd8117e1e073c0550915f58222a9bacbc57a
KPZNet <kpznet@outlook.com>
2015-07-16 17:27:52 -0700

    Updates

--- BalancePro/BalancePlaneView.swift
+++ BalancePro/BalancePlaneView.swift
@@ -447,6 +447,44 @@ class BalancePlaneView: UIView {
     }
     
     
+    func DrawBlankRect(){
+        
+        var r = self.bounds
+        /* Create the path first. Just the path handle. */
+        let path = CGPathCreateMutable()
+        
+        /* Here are our first rectangle boundaries */
+        let rectangle1 = r //CGRect(x: 10, y: 30, width: 200, height: 300)
+        
+        /* And the second rectangle */
+        let rectangle2 = r //CGRect(x: 40, y: 100, width: 90, height: 300)
+        
+        /* Put both rectangles into an array */
+        let rectangles = [rectangle1, rectangle2]
+        
+        /* Add the rectangles to the path */
+        CGPathAddRects(path, nil, rectangles, 2)
+        
+        /* Get the handle to the current context */
+        let currentContext = UIGraphicsGetCurrentContext()
+        
+        /* Add the path to the context */
+        CGContextAddPath(currentContext, path)
+        
+        /* Set the fill color to cornflower blue */
+        UIColor.grayColor().setFill()
+        //UIColor(red: 0.20, green: 0.60, blue: 0.80, alpha: 1.0).setFill()
+        
+        /* Set the stroke color to black */
+        //UIColor.blackColor().setStroke()
+        
+        /* Set the line width (for the stroke) to 5 */
+        //CGContextSetLineWidth(currentContext, 5)
+        
+        /* Stroke and fill the path on the context */
+        CGContextDrawPath(currentContext, kCGPathFillStroke)
+    }
+    
     
     override func drawRect(rect: CGRect)
     {
