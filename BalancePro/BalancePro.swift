1faee91b148ef10e30ce93776e0bad7d6c313430
KPZNet <kpznet@outlook.com>
2015-09-17 10:03:26 -0700

    Cleanup

--- BalancePro/BalancePro.swift
+++ BalancePro/BalancePro.swift
@@ -19,6 +19,50 @@ func GetAppDelegate() -> AppDelegate
     return appDelegate
 }
 
+func CalcAngleDegreesBetweenVectors(_v1:Vector, _v2:Vector) -> Float
+{
+    var v1Phase = _v1.phase
+    var v2Phase = _v2.phase
+    
+    var deg = Float(v1Phase - v2Phase)
+    if deg < 0.0 { deg += 360 }
+    
+    return deg
+}
+
+func CalcBalanceWeight() -> BalanceWeight
+{
+    var balWeight:BalanceWeight = BalanceWeight()
+    
+    if let initVect = GetAppDelegate().singlePlaneBalance.initialVector
+    {
+        if let inflVect = GetAppDelegate().singlePlaneBalance.influenceVector
+        {
+            if let wP = GetAppDelegate().singlePlaneBalance.influenceBalanceWeight
+            {
+                var inflVectOrigin = Vector(fromAmp: inflVect.amp, fromPhaseInDegrees: inflVect.phase, withRunType:BalanceRunType.influenceOrigin)
+                
+                var initVectNeg = -1*initVect
+                
+                var influenceVectOrigAmp = inflVectOrigin.amp
+                var initVectAmp = initVect.amp
+                
+                var balanceWeightRatio:Float = wP.weight/influenceVectOrigAmp
+                var bW = balanceWeightRatio * initVectAmp
+                
+                let deg = CalcAngleDegreesBetweenVectors(initVectNeg, inflVectOrigin)
+                let wL = (wP.location + deg) % 360
+                
+                var bWeight:BalanceWeight = BalanceWeight(fromWeight: bW, fromLocation: wL)
+                
+                balWeight = bWeight
+            }
+        }
+    }
+    
+    return balWeight
+}
+
 
 class SinglePlaneVectorBalance {
     
